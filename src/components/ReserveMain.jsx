7b36d8be940abc5a4419fc5f233734ba95cdfc0f
Max Mena <raemxam@gmail.com>
2023-05-05 16:50:56 -0600

    Create ReserveMain component to simplify Main

---
+++ src/components/ReserveMain.jsx
@@ -0,0 +1,29 @@
+import Button from "./Button";
+import reservationImage from "./img/gourmet_dish_1.jpg";
+
+function ReserveMain() {
+  return (
+    <section className="reservation" id="reservation">
+      <article></article>
+      <article className="left">
+        <h2>Little Lemon</h2>
+        <h3>Chicago</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
+          dolores sit animi dolore expedita tempore. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Iste sit quasi, voluptatibus beatae
+          perferendis in repellat eius laboriosam sed rerum?
+        </p>
+        <Button text="Reserve a table" />
+      </article>
+      <article className="right">
+        <div className="imageReservation">
+          <img src={reservationImage} alt="image of a menu item" />
+        </div>
+      </article>
+      <article></article>
+    </section>
+  );
+}
+
+export default ReserveMain;
