507a35396e5ec07d859b0577d1bcae44d28e6adf
Max Mena <raemxam@gmail.com>
2023-05-05 17:05:10 -0600

    Create AboutMain to divide it from Main

---
+++ src/components/AboutMain.jsx
@@ -0,0 +1,16 @@
+import placeHolderImg from "./img/littleLemonLogo.jpg";
+
+function AboutMain() {
+  return (
+    <section className="about" id="about">
+      <div></div>
+      <article className="left">
+        <img src={placeHolderImg} alt="" />
+      </article>
+      <article className="right">right</article>
+      <div></div>
+    </section>
+  );
+}
+
+export default AboutMain;
