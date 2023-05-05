acdc3d2523a94d0253be2e2603b4cd3b3dd36165
Max Mena <raemxam@gmail.com>
2023-05-05 16:42:53 -0600

    apply the same format to all footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,17 +32,29 @@ function Footer() {
       <article>
         <h2>Contact</h2>
         <ul>
-          <li>Address</li>
-          <li>Phone number</li>
-          <li>Email</li>
+          <li>
+            <a href="#">Address</a>
+          </li>
+          <li>
+            <a href="#">Phone number</a>
+          </li>
+          <li>
+            <a href="#">Email</a>
+          </li>
         </ul>
       </article>
       <article>
         <h2>Social Media Links</h2>
         <ul>
-          <li>Facebook</li>
-          <li>Instagram</li>
-          <li>linkedin</li>
+          <li>
+            <a href="#">Facebook</a>
+          </li>
+          <li>
+            <a href="#">Instagram</a>
+          </li>
+          <li>
+            <a href="#">linkedin</a>
+          </li>
         </ul>
       </article>
     </footer>
